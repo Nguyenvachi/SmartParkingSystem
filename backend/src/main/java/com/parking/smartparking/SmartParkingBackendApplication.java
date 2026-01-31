94d1429799218f8917df56313cd8b7b4efdcb4ad
Nguyenvachi <tn822798@gmail.com>
2026-01-31 23:17:13 +0700

    Khoi tao du an: Backend Spring Boot + Frontend Structure

---
+++ backend/src/main/java/com/parking/smartparking/SmartParkingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.parking.smartparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkingBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/parking/smartparking/SmartParkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.smartparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
